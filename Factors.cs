974dba112af6ef70e53c9b2af940267dd63f9e6e
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-08 14:50:31 +0530

    Added logic to find prime factors

---
+++ Factors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Factors
+    {
+        public void factorsLogic()
+        {
+            //Getting input from user
+            Console.Write("Please Enter The Number to check for prime factors: ");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.Write("Factors= 1");
+            //Logic to print out prime factors for the given number
+            for (int i = 2; i * i <= inputNum; i++)
+            {                                                                     
+                while (inputNum % i == 0)
+                    
+                {                                                    
+                    Console.Write(i + " ");                
+                    inputNum = inputNum / i;
+                }
+            }
+
+            // For the number when after the loop and finding all the prime factors and to print the number itself as it also is a factor            
+            if (inputNum > 1)
+            {
+                Console.Write(inputNum);
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,46 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Basic Core Programs in C-Sharp");
             Console.WriteLine("");
-            //CoinFlipper coinFlipper = new CoinFlipper();
-            //coinFlipper.coinFlipLogic();
-            //LeapYear leapYear = new LeapYear();
-            //leapYear.leapYearLogic();
+            Console.WriteLine("1.Flip Coin and print percentage of Heads and Tails \n2.Leap Year \n3.Power of 2 " +
+                    "\n4.Harmonic Number \n5.Factors");
+            
+            Console.Write("\nPlease Enter Your Option to choose the program to execute : ");
 
-            //Power2 power2 = new Power2();
-            //power2.powerOf2logic();
+            int option = Convert.ToInt32(Console.ReadLine());
 
-            HarmonicNumber harmonicNumObj = new HarmonicNumber();
-            harmonicNumObj.harmonicNumberLogic();
+            switch (option)
+            {
+                case 1:
+                    //// create coinFlipper class object to accessing CoinFlipper class.  
+                    CoinFlipper coinFlipper = new CoinFlipper();
+                    coinFlipper.coinFlipLogic();
+                    break;
+                case 2:
+                    //// create leapYear class object to accessing LeapYear class. 
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.leapYearLogic();
+                    break;
+
+                case 3:
+                    //// create power2 class object to accessing Power2 class. 
+                    Power2 power2 = new Power2();
+                    power2.powerOf2logic();
+                    break;
+                case 4:
+                    //// create harmonicNumObj class object to accessing HarmonicNumber class. 
+                    HarmonicNumber harmonicNumObj = new HarmonicNumber();
+                    harmonicNumObj.harmonicNumberLogic();
+                    break;
+                case 5:
+                    //// create primeFactors class object to accessing Factors class. 
+                    Factors primeFactors = new Factors();
+                    primeFactors.factorsLogic();
+                    break;                
+                default:
+                    Console.WriteLine("Wrong option! please enter option again.");
+                    break;
+            }
+
             Console.ReadKey();
         }
     }
