c230545fb0c80366b4db4a0d1cb48885e635d6ba
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-08 16:03:01 +0530

    Added logic to swap two numbers

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Basic Core Programs in C-Sharp");
             Console.WriteLine("");
-            Console.WriteLine("1.Flip Coin and print percentage of Heads and Tails \n2.Leap Year \n3.Power of 2 " + "\n4.Harmonic Number \n5.Factors \n6.Program to Compute Quotient and Remainder");
+            Console.WriteLine("1.Flip Coin and print percentage of Heads and Tails \n2.Leap Year \n3.Power of 2 " + "\n4.Harmonic Number \n5.Factors \n6.Program to Compute Quotient and Remainder \n7.Program to Swap Two Numbers");
             
             Console.Write("\nPlease Enter Your Option to choose the program to execute : ");
 
@@ -17,34 +17,40 @@ namespace BasicCorePrograms
             switch (option)
             {
                 case 1:
-                    //// create coinFlipper class object to accessing CoinFlipper class.  
+                    //// create coinFlipper class object to access CoinFlipper class.  
                     CoinFlipper coinFlipper = new CoinFlipper();
                     coinFlipper.coinFlipLogic();
                     break;
                 case 2:
-                    //// create leapYear class object to accessing LeapYear class. 
+                    //// create leapYear class object to access LeapYear class. 
                     LeapYear leapYear = new LeapYear();
                     leapYear.leapYearLogic();
                     break;
 
                 case 3:
-                    //// create power2 class object to accessing Power2 class. 
+                    //// create power2 class object to access Power2 class. 
                     Power2 power2 = new Power2();
                     power2.powerOf2logic();
                     break;
                 case 4:
-                    //// create harmonicNumObj class object to accessing HarmonicNumber class. 
+                    //// create harmonicNumObj class object to access HarmonicNumber class. 
                     HarmonicNumber harmonicNumObj = new HarmonicNumber();
                     harmonicNumObj.harmonicNumberLogic();
                     break;
                 case 5:
-                    //// create primeFactors class object to accessing Factors class. 
+                    //// create primeFactors class object to access Factors class. 
                     Factors primeFactors = new Factors();
                     primeFactors.factorsLogic();
                     break;
                 case 6:
+                    //// create computeObj class object to access RemainderQuotient class. 
                     RemainderQuotient computeObj = new RemainderQuotient();
                     computeObj.CalculationLogic();
+                    break;
+                case 7:
+                    //// create swapObj class object to access SwapNumbers class. 
+                    SwapNumbers swapObj = new SwapNumbers();
+                    swapObj.swapLogic();
                     break;
                 default:
                     Console.WriteLine("Wrong option/entry, please enter option again.");
---
+++ SwapNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class SwapNumbers
+    {
+        public void swapLogic()
+        {
+            //Taking in numeric inputs from user
+            Console.Write("Please Enter number n1 : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please Enter number n2 : ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swap the numbers are n1= " + n1 + " n2= " + n2);
+
+            //Logic to swap the numbers without a third variable
+
+            n1 = n1 * n2;
+
+            n2 = n1 / n2;
+
+            n1 = n1 / n2;
+
+            Console.WriteLine("After swap the numbers are n1= " + n1 + " n2= " + n2);
+
+
+        }
+
+    }
+}
