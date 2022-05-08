1dc28af7301a0eb3136db785c22c8eb89f361694
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-08 13:32:35 +0530

    Added logic for caculating harmonic number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class HarmonicNumber
+    {
+        public void harmonicNumberLogic()
+        {
+            //Taking in number input from user
+            Console.Write("Please Enter a Number greater than zero : ");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+
+
+            //compute 1/1 + 1/2 + 1/3 + ... + 1/N
+            for (int i = 1; i <= inputNum; i++)
+            {                                                            
+                sum = sum + ((double)1 / i);                            
+            }
+
+            Console.Write("The Harmonic number is: "+sum);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,8 +13,11 @@ namespace BasicCorePrograms
             //LeapYear leapYear = new LeapYear();
             //leapYear.leapYearLogic();
 
-            Power2 power2 = new Power2();
-            power2.powerOf2logic();
+            //Power2 power2 = new Power2();
+            //power2.powerOf2logic();
+
+            HarmonicNumber harmonicNumObj = new HarmonicNumber();
+            harmonicNumObj.harmonicNumberLogic();
             Console.ReadKey();
         }
     }
