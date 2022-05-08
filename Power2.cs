cbdc1c6197d4904e5c6f24b37b7c18c45660ee8c
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-08 13:15:19 +0530

    Added program to caluculate the power of two

---
+++ Power2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Power2
+    {
+        public void powerOf2logic()
+        {
+            //Taking in number input from user
+            Console.Write("Please Enter a Number between 0 and 31 : ");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            //Simple for loop to print out all the power values of two
+            if(0 <= inputNum && inputNum < 31)
+            {
+                for (int i = 0; i <= inputNum; i++)
+                {
+                    Console.WriteLine("2 ^ {0} = {1}", i, Math.Pow(2,i ));
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wront Input, Please restart the program and enter a Number between 0 and 31");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace BasicCorePrograms
             Console.WriteLine("");
             //CoinFlipper coinFlipper = new CoinFlipper();
             //coinFlipper.coinFlipLogic();
-            LeapYear leapYear = new LeapYear();
-            leapYear.leapYearLogic();
+            //LeapYear leapYear = new LeapYear();
+            //leapYear.leapYearLogic();
+
+            Power2 power2 = new Power2();
+            power2.powerOf2logic();
             Console.ReadKey();
         }
     }
